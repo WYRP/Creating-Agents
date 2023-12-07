bb2eec91c833187e9f4df08ae3f12c080c9f2248
Yongru <yongru.pan@mail.mcgill.ca>
2023-12-06 19:31:33 -0500

    Done

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -1,18 +1,16 @@
 # Student agent: Add your own agent here
 import math
 
-import world
-from agents.agent import Agent
 from store import register_agent
 import sys
 import numpy as np
 from copy import deepcopy
 import time
-
+from agents.agent import Agent
 
 from queue import Queue
 import time
-from world import World  # Import the World class
+
 
 
 @register_agent("student_agent")
@@ -20,46 +18,63 @@ class StudentAgent(Agent):
     def __init__(self):
         super(StudentAgent, self).__init__()
         self.name = "StudentAgent"
-        self.dir_map = {"u": 0, "r": 1, "d": 2, "l": 3}
+        self.moves = ((-1, 0), (0, 1), (1, 0), (0, -1))  # Up, Right, Down, Left
 
+    def check_boundary(self, pos, board_size):
+        r, c = pos
+        return 0 <= r < board_size and 0 <= c < board_size
 
-    def bfs_with_heuristic(self, chess_board, my_pos, adv_pos, max_step):
+    def bfs_search(self, chess_board, start_pos, max_step):
         queue = Queue()
-        queue.put((my_pos, 0))  # (position, depth)
-        best_score = float('-inf')
-        best_move = None
+        queue.put((start_pos, 0))  # (position, depth)
+        visited = set([start_pos])
 
         while not queue.empty():
             current_pos, depth = queue.get()
             if depth >= max_step:
                 continue
 
-            for move in get_legal_moves(chess_board, current_pos, max_step):
-                new_pos = apply_move(current_pos, move)
-                score = self.heuristic(chess_board, new_pos, adv_pos)
-                
-                if score > best_score:
-                    best_score = score
-                    best_move = move
+            for move in self.moves:
+                new_pos = (current_pos[0] + move[0], current_pos[1] + move[1])
 
-                if not is_terminal(chess_board, new_pos):
-                    queue.put((new_pos, depth + 1))
+                # Check for boundary and avoid revisiting
+                if new_pos in visited or not self.check_boundary(new_pos, chess_board.shape[0]):
+                    continue
 
-        return best_move
+                # Check if the position is free of barriers
+                if not chess_board[new_pos[0], new_pos[1]].any():  # Assuming chess_board is a numpy array
+                    return new_pos  # Valid move found
+
+                visited.add(new_pos)
+                queue.put((new_pos, depth + 1))
+
+        return start_pos  # If no move found, stay in place
     
-    def is_move_valid(self, chess_board, best_move, barrier_dir):
-        # Adapt the logic from check_valid_step here
-        # You may need to adjust the parameters and logic to match your agent's needs
-        return valid  # Return True if the move is valid, False otherwise
+    def heuristic(self, chess_board, new_pos, adv_pos):
 
+        best_dir = 1  
+        max_distance = -1
+
+        for dir, move in enumerate(self.moves):
+            adjacent_pos = (new_pos[0] + move[0], new_pos[1] + move[1])
+
+            # Check if the adjacent position is within boundaries and not blocked
+            if not self.check_boundary(adjacent_pos, chess_board.shape[0]) or chess_board[adjacent_pos[0], adjacent_pos[1]].any():
+                continue
+
+            # Calculate distance to adversary from this adjacent position
+            distance_to_adv = np.linalg.norm(np.array(adjacent_pos) - np.array(adv_pos))
+
+            # Select the direction that maximizes distance from the adversary
+            if distance_to_adv > max_distance:
+                max_distance = distance_to_adv
+                best_dir = dir
+
+        return best_dir
+    
     def step(self, chess_board, my_pos, adv_pos, max_step):
-        start_time = time.time()
-        # Call BFS with Heuristic
-        best_move = self.bfs_with_heuristic(chess_board, my_pos, adv_pos, max_step)
-        time_taken = time.time() - start_time
-        print("My AI's turn took ", time_taken, "seconds.")
+        new_pos = self.bfs_search(chess_board, my_pos, max_step)
+        barrier_dir = self.heuristic(chess_board, new_pos, adv_pos)
+        return new_pos, barrier_dir
 
-        return best_move, self.dir_map["u"]
-
-current_player_obj, current_player_pos, adversary_player_pos = world.get_current_positions()
 
